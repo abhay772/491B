dae8c330edd2c6ec45dcd8e0dda73c2a3ea36a78
abhay772 <absolanki772@gmail.com>
2023-02-20 18:04:19 -0800

    Add: Authentication, Account Recovery

---
+++ Backend/PMTOGO.WebAPP/AA.PMTOGO_v2/AA.PMTOGO_v2/Program.cs
@@ -0,0 +1,80 @@
+namespace AA.PMTOGO_v2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateWebAPI(args);
+    }
+
+    public static void CreateWebAPI(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var startup = new Startup(builder.Configuration);
+
+        startup.ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        startup.Configure(app, builder.Environment);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
