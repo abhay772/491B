e7e42d90610845ab904caef726deaa827a8b759c
Anh Huynh <huynhanh8327@gmail.com>
2023-03-13 00:29:25 -0700

    z

--- Backend/PMTOGO.WebAPP/PMTOGO.Infrastructure/Interfaces/IUserManagement.cs
+++ Backend/PMTOGO.WebAPP/PMTOGO.Infrastructure/Interfaces/IUserManagement.cs
@@ -10,6 +10,7 @@ namespace AA.PMTOGO.Infrastructure.Interfaces
     public interface IUserManagement
     {
         Task<Result> CreateAccount(string email, string password, string firstname, string lastname, string role);
+        Task<Result> AddServiceToUser(string username, Service service);
         Task<Result> DeactivateAccount(string username, string password);
         string GenerateSalt();
         string EncryptPassword(string password, string salt);
