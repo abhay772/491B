df013b814d683d82cd0855dd83e19c2f44a16f7f
abhay772 <absolanki772@gmail.com>
2023-03-13 18:12:35 -0700

    Bugfix

--- Backend/PMTOGO.WebAPP/PMTOGO.Domain/Entities/UserService.cs
+++ Backend/PMTOGO.WebAPP/PMTOGO.Domain/Entities/UserService.cs
@@ -1,31 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 
-namespace AA.PMTOGO.Models.Entities;
 
-public class UserService
+namespace AA.PMTOGO.Models.Entities
 {
+
     public class UserService
     {
-        [Key]
-        public Guid ServiceId { get; set; }
-        [Key]
-        public string PropertyManagerEmail { get; set; } = string.Empty;
-        public string PropertyManagerName { get; set; } = string.Empty;
-        public string ServiceName { get; set; } = string.Empty;
-        public string ServiceDescription { get; set; } = string.Empty;
-        public string ServiceType { get; set; } = string.Empty;
-        public string ServiceFrequency { get; set; } = string.Empty;
-        [Required]
-        public string ServiceProviderEmail { get; set; } = string.Empty;
-        public string ServiceProvider { get; set; } = string.Empty;
-        public string Status { get; set; } = string.Empty;
-        public int Rating { get; set; }
         [Key]
         public Guid ServiceId { get; set; }
         [Key]
@@ -57,19 +38,8 @@ public class UserService
             PropertyManagerName = propertyManagerName;
 
         }
-        public UserService(Guid serviceId, string serviceName, string serviceDescription, string serviceType, string serviceFrequency, string serviceProviderEmail, string serviceProvider, string propertyManagerEmail, string propertyManagerName, string status, int rating)
-    }
-}            ServiceName = serviceName;
-            ServiceType = serviceType;
-            ServiceDescription = serviceDescription;
-            ServiceFrequency = serviceFrequency;
-            ServiceProviderEmail = serviceProviderEmail;
-            ServiceProvider = serviceProvider;
-            PropertyManagerEmail = propertyManagerEmail;
-            PropertyManagerName = propertyManagerName;
-            Status = status;
-            Rating = rating;
+
         }
 
     }
-}
+
