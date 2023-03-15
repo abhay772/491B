6547e3e011cae66f457655a322e5ceff654564ed
abhay772 <absolanki772@gmail.com>
2023-03-15 11:48:49 -0700

    Update UserService.cs

--- Backend/PMTOGO.WebAPP/PMTOGO.Domain/Entities/UserService.cs
+++ Backend/PMTOGO.WebAPP/PMTOGO.Domain/Entities/UserService.cs
@@ -6,6 +6,7 @@ public class UserService
 {
     public Guid ServiceId { get; set; }
     public string PropertyManagerEmail { get; set; } = string.Empty;
+    public string PropertyManagerName { get; private set; }
     public string ServiceName { get; set; } = string.Empty;
     public string ServiceDescription { get; set; } = string.Empty;
     public string ServiceType { get; set; } = string.Empty;
@@ -16,26 +17,6 @@ public class UserService
     public int Rating { get; set; }
 
     public UserService() { }
-
-   public UserService(Guid serviceId, string propertyManagerEmail, string serviceName, string serviceDescription,
-       string serviceType, string serviceFrequency, string serviceProviderEmail, string serviceProvider, string status, int rating)
-    {
-        [Key]
-        public Guid ServiceId { get; set; }
-        [Key]
-        public string PropertyManagerEmail { get; set; } = string.Empty;
-        public string PropertyManagerName { get; set; } = string.Empty;
-        public string ServiceName { get; set; } = string.Empty;
-        public string ServiceDescription { get; set; } = string.Empty;
-        public string ServiceType { get; set; } = string.Empty;
-        public string ServiceFrequency { get; set; } = string.Empty;
-        [Required]
-        public string ServiceProviderEmail { get; set; } = string.Empty;
-        public string ServiceProvider { get; set; } = string.Empty;
-        public string Status { get; set; } = string.Empty;
-        public int Rating { get; set; }
-
-        public UserService() { }
 
        public UserService(Guid serviceId, string serviceName, string serviceDescription, string serviceType,
            string serviceFrequency, string serviceProviderEmail, string serviceProvider, string propertyManagerEmail, string propertyManagerName)
