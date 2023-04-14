5debc33945f293e6bf689b6c9e16b4be496147a4
Anh Huynh <huynhanh8327@gmail.com>
2023-04-14 13:47:22 -0700

    vscode swagge ui

--- Backend/PMTOGO.WebAPP/AA.PMTOGO.WebAPP/Program.cs
+++ Backend/PMTOGO.WebAPP/AA.PMTOGO.WebAPP/Program.cs
@@ -25,7 +25,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AA.PMTOGO V1");
+        c.RoutePrefix = "";
+    });
 }
 
 app.UseMiddleware<CorsMiddleware>();
