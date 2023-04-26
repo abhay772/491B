41f6ffa504404c544bf4874041b48145754af803
Sierra Harris <mssierra310@gmail.com>
2023-04-25 22:37:01 -0700

    started service management e2e test

--- Frontend/nightwatch_tests/tests/e2e_request_test.js
+++ Frontend/nightwatch_tests/tests/e2e_request_test.js
@@ -11,46 +11,30 @@ describe('Login Success', function() {
             .setValue('input[type=password', 'randomstring')
             .pause()
             .click('button[class="loginbtn"]')
+            .waitForElementVisible('button[type="submit"]', 5000)
+            .click('button[type="submit"]')
+            .click('.servicerequest-linkc div:nth-child(2) button')
+            .waitForElementVisible('#NewRequest', 10000)
+            .setValue('input[name="frequencies"]', "month")
+            .setValue('input[name="times"]', 3)
+            .setValue('input[name="comments"]', "e2e new request test")
+            .click('#requestservice')
+            .waitForElementVisible('#homepage',5000)
 
-            .perform(function () {
-                duration = Date.now();
-            })
-            .waitForElementVisible('homepage', function() {
-                duration = (Date.now() - duration)/1000;
-            })
-            .perform(function() {
-                if(duration > 5){
-                    throw new Error('NFR failed')
-                }
-            })
-            .assert.urlContains("/homepage.html")
+            .click('#serviceManagement')
+            .waitForElementVisible('div.userservices', 10000)
+            .click('.rate-linkc div:nth-child(1) button')
+            .waitForElementVisible('#rate-form', 10000)
+            .setValue('input[name="rating"]', 4)
+            .click('#rateservice')
+            .waitForElementVisible('div.userservices',5000)
+            .assert.notEqual(browser.getText('div.userservices'),'00000.00') 
+            .end();
+            
     });
 
     after(browser => browser.end());
 
 });
 
-module.exports = {
-    'Accept request test': function(browser) {
-        browser
-        .url("http://localhost:6000/")
-        .waitForElementVisible('form',5000)
-        .click('button[type="submit"]')
-        .waitForElementVisible('#property-profile', 5000) 
-        .setValue('input[name="noOfBedrooms"]', 1)
-        .setValue('input[name="noOfBathrooms"]', 1)
-        .setValue('input[name="sqFeet"]', 1)
-        .setValue('input[name="Address1"]', "Somewhere somehow")
-        .setValue('input[name="Address2"]', "#491")
-        .setValue('input[name="City"]', "Long Beach")
-        .setValue('input[name="State"]', "CA")
-        .setValue('input[name="Zip"]', "90815")
-        .setValue('input[name="Desciption"]', 
-        "Just testing the description.")
-        .click('#evaluate-property-btn')
-        .waitForElementVisible('#property-evaluation',5000)
-        .assert.notEqual(browser.getText('#property-evaluation'),'00000.00') 
-        .end();
-    }
-}
 
